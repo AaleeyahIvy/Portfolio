de59fb0e8f3a234e19bd48bbd67308d5878910fd
Aaleeyah Ivy <53322518+AaleeyahIvy@users.noreply.github.com>
2022-07-21 15:38:45 -0400

    7212022

--- Project/functions.js
+++ Project/functions.js
@@ -1,10 +1,22 @@
-var btns = document.getElementsByClassName("button");
+/*var btns = document.getElementsByClassName("button");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(" active", "");
   this.className += " active";
   });
+}
+*/
+function isMarried (){
+  const today = new Date()
+  const date = new Date(2022/9/11)
+  messageOne = "My amazing husband:&nbsp;&nbsp;Austin Kilgore helped me with the server,&nbsp; &nbsp;thank you.";
+  messageTwo = "My amazing future husband:&nbsp;&nbsp;Austin Kilgore helped me with the server,&nbsp; &nbsp;thank you.";
+  if (today === 9/11/2022){
+   document.getElementsByClassName("message") = messageOne;
+  } else{
+    document.getElementsByClassName("message") = messageTwo;
+  }
 }
 
 
